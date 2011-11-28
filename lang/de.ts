794d9a60c5a6778e7e5980c1486a0cd62904c77d
Shilyaev Egor <egor.shilyaev@gmail.com>
2011-11-28 10:39:41 +0500

    added: Забыл довать файлы перевода и словари.

---
+++ lang/de.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>RSSListing</name>
+    <message>
+        <location filename="../rsslisting.cpp" line="157"/>
+        <source>Item</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="157"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="157"/>
+        <source>Link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="157"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="158"/>
+        <source>Comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="158"/>
+        <source>pubDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="158"/>
+        <source>guid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="265"/>
+        <source>&amp;Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="266"/>
+        <source>Add new feed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="269"/>
+        <source>&amp;Delete...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="270"/>
+        <source>Delete selected feed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="276"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="282"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="286"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="287"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="288"/>
+        <source>Fee&amp;ds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="289"/>
+        <source>&amp;News</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="291"/>
+        <source>&amp;Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="294"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="299"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="308"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="313"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rsslisting.cpp" line="317"/>
+        <source>Russian</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
