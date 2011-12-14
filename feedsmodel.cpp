7d1b8eea6ab2a40f9e3815994ecef8a1fcdcf676
Shilyaev Egor <egor.shilyaev@gmail.com>
2011-12-14 15:05:10 +0500

    changed: В таблице лент количество непрочитанных отображается в скобках, а если их ноль то не отображается.

--- feedsmodel.cpp
+++ feedsmodel.cpp
@@ -12,10 +12,25 @@ QVariant FeedsModel::data(const QModelIndex &index, int role) const
 {
   if (role == Qt::FontRole) {
     QFont font;
-    if (0 < QSqlTableModel::index(index.row(), fieldIndex("unread")).data().toInt())
+    if (0 < QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt())
       font.setBold(true);
     return font;
+  } else if (role == Qt::DisplayRole){
+    if (QSqlTableModel::fieldIndex("unread") == index.column()) {
+      if (0 == QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt()) {
+        return QVariant();
+      } else {
+        QString qStr = QString("(%1)").
+            arg(QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt());
+        return qStr;
+      }
+    }
   }
 
   return QSqlTableModel::data(index, role);
+}
+
+/*virtual*/ bool	FeedsModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+  return QSqlTableModel::setData(index, value, role);
 }
