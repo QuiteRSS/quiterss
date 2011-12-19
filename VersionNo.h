c9f035d682d5ae57751e28c1e725fb0cb2396e48
Shilyaev Egor <egor.shilyaev@gmail.com>
2011-12-19 14:32:57 +0600

    version: updated to 0.4.0.0

--- VersionNo.h
+++ VersionNo.h
@@ -1,4 +1,4 @@
-#define FILEVER        0,3,1,0
+#define FILEVER        0,4,0,0
 #define PRODUCTVER     FILEVER
-#define STRFILEVER     "0.3.1.0\0"
+#define STRFILEVER     "0.4.0.0\0"
 #define STRPRODUCTVER  STRFILEVER
