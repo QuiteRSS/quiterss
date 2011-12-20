6d3463c36cab86776ed787ef963bfd1ecc51f87f
Aleksey Khokhryakov <aleksey.khokhryakov@gmail.com>
2011-12-20 13:10:45 +0500

    changed: Поле даты публикиции новости хранится и обрабатывается в UTC формате

--- newsmodel.cpp
+++ newsmodel.cpp
@@ -29,10 +29,23 @@ QVariant NewsModel::data(const QModelIndex &index, int role) const
   } else if (role == Qt::DisplayRole) {
     if (QSqlTableModel::fieldIndex("read") == index.column()) {
       return QVariant();
-    } else if (QSqlTableModel::fieldIndex("sticky") == index.column()) {
+    }
+    else if (QSqlTableModel::fieldIndex("sticky") == index.column()) {
       return QVariant();
-    } else if (QSqlTableModel::fieldIndex("received") == index.column()) {
-      qDebug() << QSqlTableModel::index(index.row(), fieldIndex("received")).data(Qt::EditRole).toString();
+    }
+    else if (QSqlTableModel::fieldIndex("published") == index.column()) {
+      QString strPublished;
+      strPublished = QSqlTableModel::index(index.row(), fieldIndex("published")).data(Qt::EditRole).toString();
+      QLocale locale(QLocale::C);
+      QDateTime dt = locale.toDateTime(strPublished, "yyyy-MM-ddTHH:mm:ss");
+      int daysToCurrentDT = dt.daysTo(QDateTime::currentDateTimeUtc());
+      if (daysToCurrentDT < 0) {
+        qWarning() << "News has future published Date";
+        return QString();
+      }
+      else return dt.toString("yyyy.MM.dd hh:mm");
+    }
+    else if (QSqlTableModel::fieldIndex("received") == index.column()) {
       QDateTime dateTime = QDateTime::fromString(
             QSqlTableModel::index(index.row(), fieldIndex("received")).data(Qt::EditRole).toString(),
             Qt::ISODate);
