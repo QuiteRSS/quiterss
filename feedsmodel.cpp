fe04b182cb20f136489ef57dd8d5184e59873510
Shilyaev Egor <egor.shilyaev@gmail.com>
2011-12-21 20:16:47 +0600

    changed: В дереве лент количество не прочитанных новостей отображается синим и не жирным (вроде получше)

--- feedsmodel.cpp
+++ feedsmodel.cpp
@@ -1,5 +1,6 @@
 #include <QDebug>
 #include <QFont>
+#include <QBrush>
 
 #include "feedsmodel.h"
 
@@ -12,7 +13,8 @@ QVariant FeedsModel::data(const QModelIndex &index, int role) const
 {
   if (role == Qt::FontRole) {
     QFont font;
-    if (0 < QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt())
+    if ((0 < QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt()) &&
+        (QSqlTableModel::fieldIndex("unread") != index.column()))
       font.setBold(true);
     return font;
   } else if (role == Qt::DisplayRole){
@@ -25,6 +27,12 @@ QVariant FeedsModel::data(const QModelIndex &index, int role) const
         return qStr;
       }
     }
+  } else if (role == Qt::TextColorRole) {
+    QBrush brush;
+    if (QSqlTableModel::fieldIndex("unread") == index.column()) {
+      brush.setColor("#0000CA");
+    }
+    return brush;
   }
 
   return QSqlTableModel::data(index, role);
