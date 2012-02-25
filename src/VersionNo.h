51dc340bbb490c5687457955e615cf892578169a
Shilyaev Egor <egor.shilyaev@gmail.com>
2012-02-25 21:41:52 +0600

    version: Updated to 0.8.3 (25.02.2012)

--- src/VersionNo.h
+++ src/VersionNo.h
@@ -2,8 +2,8 @@
 #define PORTABLE       1
 #endif
 
-#define FILEVER        0,8,2,0
+#define FILEVER        0,8,3,0
 #define PRODUCTVER     FILEVER
-#define STRFILEVER     "0.8.2.0\0"
+#define STRFILEVER     "0.8.3.0\0"
 #define STRPRODUCTVER  STRFILEVER
-#define STRDATE        "10.02.2012\0"
+#define STRDATE        "25.02.2012\0"
