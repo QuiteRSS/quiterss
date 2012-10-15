1476906c52072d0fc0a2798edc949a4b2fced548
Shilyaev Egor <egor.shilyaev@gmail.com>
2012-10-15 16:00:17 +0600

    fixed: Не менялся тип шрифта панели новости.

--- src/newstabwidget.cpp
+++ src/newstabwidget.cpp
@@ -418,13 +418,13 @@ void NewsTabWidget::setSettings(bool newTab)
     }
 
     webPanelTitleLabel_->setFont(
-          QFont(rsslisting_->newsFontFamily_, rsslisting_->panelNewsFontSize_));
+          QFont(rsslisting_->panelNewsFontFamily_, rsslisting_->panelNewsFontSize_));
     webPanelTitle_->setFont(
-          QFont(rsslisting_->newsFontFamily_, rsslisting_->panelNewsFontSize_));
+          QFont(rsslisting_->panelNewsFontFamily_, rsslisting_->panelNewsFontSize_));
     webPanelAuthorLabel_->setFont(
-          QFont(rsslisting_->newsFontFamily_, rsslisting_->panelNewsFontSize_));
+          QFont(rsslisting_->panelNewsFontFamily_, rsslisting_->panelNewsFontSize_));
     webPanelAuthor_->setFont(
-          QFont(rsslisting_->newsFontFamily_, rsslisting_->panelNewsFontSize_));
+          QFont(rsslisting_->panelNewsFontFamily_, rsslisting_->panelNewsFontSize_));
 
     webView_->settings()->setFontFamily(
           QWebSettings::StandardFont, rsslisting_->webFontFamily_);
