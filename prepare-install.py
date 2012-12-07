d5f160f26d2c14dec7d486cd5a8af0261af7e569
Aleksey Khokhryakov <aleksey.khokhryakov@gmail.com>
2012-12-07 15:23:39 +0600

    added: Копирование файлов перевода

--- prepare-install.py
+++ prepare-install.py
@@ -7,21 +7,30 @@
 '''
 
 import os
+import shutil
 
-prepareAbsPath = "e:\\Work\\_Useful\\QtProjects\\QuiteRSS_prepare-install"
+quiterssReleaseAbsPath = "e:\\Work\\_Useful\\QtProjects\\QuiteRSS-build-desktop_Release\\release\\target"
+prepareAbsPath  = "e:\\Work\\_Useful\\QtProjects\\QuiteRSS_prepare-install"
 
-def preparePath(path)
-  print "Preparing path: " + path
+def preparePath(path):
+  print "---- Preparing path: " + path
   if (os.path.exists(path)):
     print "Path exists"
   else:
     os.makedirs(path)
     print "Path created"
+    
+def copyLangFiles():
+  print "---- Copying language files..."
+  shutil.copytree(quiterssReleaseAbsPath + "\\lang", prepareAbsPath + "\\lang")
+  print "Done"
+
 
 
 def main():
   print "QuiteRSS prepare-install"
   preparePath(prepareAbsPath)
+  copyLangFiles()
 
 if __name__ == '__main__':
   main()
