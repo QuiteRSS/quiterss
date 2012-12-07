157af9db767d71734b1eb9a78880af4190efb4ff
Aleksey Khokhryakov <aleksey.khokhryakov@gmail.com>
2012-12-07 16:48:25 +0600

    added: Копирование звуков и списка файлов из исходников

--- prepare-install.py
+++ prepare-install.py
@@ -9,8 +9,18 @@
 import os
 import shutil
 
+quiterssSourceAbsPath = "e:\\Work\\_Useful\\QtProjects\\QuiteRSS"
 quiterssReleaseAbsPath = "e:\\Work\\_Useful\\QtProjects\\QuiteRSS-build-desktop_Release\\release\\target"
 prepareAbsPath  = "e:\\Work\\_Useful\\QtProjects\\QuiteRSS_prepare-install"
+
+filesFromSource = [
+  '\\AUTHORS',
+  '\\COPYING',
+  '\\HISTORY_EN',
+  '\\HISTORY_RU',
+  '\\README',
+  '\\TODO'
+]
 
 def preparePath(path):
   print "---- Preparing path: " + path
@@ -30,11 +40,25 @@ def copyExeFile():
   shutil.copy(quiterssReleaseAbsPath + "\\QuiteRSS.exe", prepareAbsPath + "\\QuiteRSS.exe")
   print "Done"
 
+def copySoundFromSource():
+  print "---- Copying sound files..."
+  shutil.copytree(quiterssSourceAbsPath + "\\sound", prepareAbsPath + "\\sound")
+  print "Done"
+
+def copyFilesFromSource():
+  print "---- Copying files from source..."
+  for file in filesFromSource:
+    print "copying: " + quiterssSourceAbsPath+file
+    shutil.copy(quiterssSourceAbsPath + file, prepareAbsPath + file)
+  print "Done"
+
 def main():
   print "QuiteRSS prepare-install"
   preparePath(prepareAbsPath)
   copyLangFiles()
   copyExeFile()
+  copySoundFromSource()
+  copyFilesFromSource()
 
 if __name__ == '__main__':
   main()
