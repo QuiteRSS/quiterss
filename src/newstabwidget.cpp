11e995d76b3b586c487465e2c622855219557132
Aleksey Khokhryakov <aleksey.khokhryakov@gmail.com>
2012-12-13 15:29:09 +0600

    refactored: Вызов функции указывается более фвно

--- src/newstabwidget.cpp
+++ src/newstabwidget.cpp
@@ -1115,7 +1115,8 @@ void NewsTabWidget::slotLinkClicked(QUrl url)
       }
       webView_->load(url);
     } else {
-      qobject_cast<WebView*>(rsslisting_->createWebTab()->view())->load(url);
+      QWebPage *webPage = rsslisting_->createWebTab();
+      qobject_cast<WebView*>(webPage->view())->load(url);
     }
   } else openUrl(url);
   webView_->midButtonClick = false;
