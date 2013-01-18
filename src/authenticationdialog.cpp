8fe9346694efe7fb41c3e1143532eb6ba1a83733
Shilyaev Egor <egor.shilyaev@gmail.com>
2013-01-18 16:58:22 +0600

    changed: Авторизация (осталось сохранять пароли)

--- src/authenticationdialog.cpp
+++ src/authenticationdialog.cpp
@@ -14,22 +14,17 @@ AuthenticationDialog::AuthenticationDialog(QWidget *parent, QNetworkReply *reply
   pass_->setEchoMode(QLineEdit::Password);
   save_ = new QCheckBox(tr("Save password"), this);
 
-  QGridLayout *serverLayout = new QGridLayout();
-  serverLayout->addWidget(new QLabel(tr("Server:")), 0, 0);
-  serverLayout->addWidget(new QLabel(reply->url().host()), 0, 1);
-  serverLayout->addWidget(new QLabel(tr("Massage:")), 1, 0);
-  serverLayout->addWidget(new QLabel(auth->realm()), 1, 1);
-  serverLayout->setColumnStretch(1, 1);
+  QGridLayout *layout = new QGridLayout();
+  layout->addWidget(new QLabel(tr("Server:")), 0, 0);
+  layout->addWidget(new QLabel(reply->url().host()), 0, 1);
+  layout->addWidget(new QLabel(tr("Massage:")), 1, 0);
+  layout->addWidget(new QLabel(auth->realm()), 1, 1);
+  layout->addWidget(new QLabel(tr("Username:")), 2, 0);
+  layout->addWidget(user_, 2, 1);
+  layout->addWidget(new QLabel(tr("Password:")), 3, 0);
+  layout->addWidget(pass_, 3, 1);
 
-  QGridLayout *userLayout = new QGridLayout();
-  userLayout->addWidget(new QLabel(tr("Username:")), 0, 0);
-  userLayout->addWidget(user_, 0, 1);
-  userLayout->addWidget(new QLabel(tr("Password:")), 1, 0);
-  userLayout->addWidget(pass_, 1, 1);
-
-  pageLayout->addLayout(serverLayout);
-  pageLayout->addSpacing(10);
-  pageLayout->addLayout(userLayout);
+  pageLayout->addLayout(layout);
   pageLayout->addSpacing(10);
   pageLayout->addWidget(save_);
 
