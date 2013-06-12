12ee8cb505ad0dd59c8c695ba6faf3828b75ae6b
Shilyaev Egor <egor.shilyaev@gmail.com>
2013-06-12 23:05:43 +0600

    refactored: Правильное написание имени приложения. Установка версии приложения

--- src/main.cpp
+++ src/main.cpp
@@ -47,8 +47,9 @@ int main(int argc, char **argv)
       return 0;
   }
 
-  app.setApplicationName("QuiteRss");
-  app.setOrganizationName("QuiteRss");
+  app.setApplicationName("QuiteRSS");
+  app.setOrganizationName("QuiteRSS");
+  app.setApplicationVersion(STRPRODUCTVER);
 #if defined(QT_NO_DEBUG_OUTPUT)
   app.setWindowIcon(QIcon(":/images/quiterss32"));
 #else
