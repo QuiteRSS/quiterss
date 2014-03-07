7834bc6a5b4f366076d77087969a5bea18258169
Shilyaev Egor <egor.shilyaev@gmail.com>
2014-03-07 23:39:12 +0600

    version: Updated to 0.15.1 (07.03.2014)

--- src/VersionNo.h
+++ src/VersionNo.h
@@ -1,9 +1,9 @@
 #include "VersionRev.h"
 
-#define STRDATE           "06.03.2014\0"
-#define STRPRODUCTVER     "0.15.0\0"
+#define STRDATE           "07.03.2014\0"
+#define STRPRODUCTVER     "0.15.1\0"
 
-#define VERSION           0,15,0
+#define VERSION           0,15,1
 #define PRODUCTVER        VERSION,0
 #define FILEVER           VERSION,VCS_REVISION
 
