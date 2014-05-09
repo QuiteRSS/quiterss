8e17adf7f186fb4bc3dcca38235cf8ff53a99bd0
Shilyaev Egor <egor.shilyaev@gmail.com>
2014-05-09 20:26:27 +0600

    changed: Окно закрытия программы вида диалог

--- src/application/mainwindow.cpp
+++ src/application/mainwindow.cpp
@@ -153,7 +153,8 @@ void MainWindow::quitApp()
   isMinimizeToTray_ = true;
   disconnect(this);
 
-  QWidget *widget = new QWidget(0, Qt::Tool | Qt::WindowTitleHint | Qt::WindowStaysOnTopHint);
+  QWidget *widget = new QWidget(0, Qt::Dialog | Qt::WindowTitleHint |
+                                Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint);
   widget->setFocusPolicy(Qt::NoFocus);
   QVBoxLayout *layout = new QVBoxLayout(widget);
   layout->addWidget(new QLabel(tr("Saving data...")));
