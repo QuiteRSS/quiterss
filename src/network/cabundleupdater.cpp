ee345be9209819f838cb405ec1f73a56c1e77b3f
Christian Stadelmann <dev@genodeftest.de>
2015-06-23 20:16:40 +0200

    Update GPL licence URL to use HTTPS

--- src/network/cabundleupdater.cpp
+++ src/network/cabundleupdater.cpp
@@ -13,7 +13,7 @@
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 * ============================================================ */
 /* ============================================================
 * QupZilla - WebKit based browser
@@ -30,7 +30,7 @@
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 * ============================================================ */
 #include "cabundleupdater.h"
 #include "mainapplication.h"
