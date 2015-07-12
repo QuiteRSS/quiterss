8b2acc2366fe677730575d0733de28efaffe5a71
Shilyaev Egor <egor.shilyaev@gmail.com>
2015-07-12 17:13:11 +0500

    version: Updated to 0.18.0 (12.07.2015)

--- src/VersionNo.h
+++ src/VersionNo.h
@@ -1,9 +1,9 @@
 #include "VersionRev.h"
 
-#define STRDATE           "19.04.2015\0"
-#define STRPRODUCTVER     "0.17.7\0"
+#define STRDATE           "12.07.2015\0"
+#define STRPRODUCTVER     "0.18.0\0"
 
-#define VERSION           0,17,7
+#define VERSION           0,18,0
 #define PRODUCTVER        VERSION,0
 #define FILEVER           VERSION,VCS_REVISION
 
