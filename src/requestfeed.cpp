90cd8b93f4999e7e30eb96b0da379c6a3b8bb1ad
Shilyaev Egor <egor.shilyaev@gmail.com>
2016-03-08 23:34:30 +0500

    fix: Некоторые ленты не обрабатывались

--- src/requestfeed.cpp
+++ src/requestfeed.cpp
@@ -178,7 +178,7 @@ void RequestFeed::slotGet(const QUrl &getUrl, const int &id, const QString &feed
 {
   qDebug() << objectName() << "::get:" << getUrl.toEncoded() << "feed:" << feedUrl;
   QNetworkRequest request(getUrl);
-  request.setRawHeader("Accept", "application/atom+xml,application/xml;q=0.9,text/xml;q=0.8,*/*;q=0.7");
+  request.setRawHeader("Accept", "application/atom+xml,application/rss+xml;q=0.9,application/xml;q=0.8,text/xml;q=0.7,*/*;q=0.6");
   request.setRawHeader("Accept-Language", "*,en-us;q=0.8,en;q=0.6");
   QString userAgent = QString("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/%1 (KHTML, like Gecko) QuiteRSS/%2 Safari/%1").
       arg(qWebKitVersion()).arg(STRPRODUCTVER);
